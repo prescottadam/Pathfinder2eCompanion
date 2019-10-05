1e4c3a4c7083d9e9792ba56cc7d9abca6539d401
Adam Prescott <adam.prescott@skykick.com>
2019-10-05 09:04:37 -0400

    refactor ability scores table to its own component

--- Pathfinder2eCompanion.Host/ClientApp/src/components/Home.js
+++ Pathfinder2eCompanion.Host/ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { AbilityScores } from './AbilityScores';
 
 export class Home extends Component {
     static displayName = Home.name;
@@ -22,34 +23,7 @@ export class Home extends Component {
             <div>
                 <h1>Pathfinder 2e Companion</h1>
                 <h2>{this.state.character.name}</h2>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>Strength</th>
-                            <td>{this.state.character.strength}</td>
-                        </tr>
-                        <tr>
-                            <th>Dexterity</th>
-                            <td>{this.state.character.dexterity}</td>
-                        </tr>
-                        <tr>
-                            <th>Constitution</th>
-                            <td>{this.state.character.constitution}</td>
-                        </tr>
-                        <tr>
-                            <th>Intelligence</th>
-                            <td>{this.state.character.intelligence}</td>
-                        </tr>
-                        <tr>
-                            <th>Wisdom</th>
-                            <td>{this.state.character.wisdom}</td>
-                        </tr>
-                        <tr>
-                            <th>Charisma</th>
-                            <td>{this.state.character.charisma}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <AbilityScores {...this.state.character}></AbilityScores>
             </div>
         );
     }
